32bdb3b3cd057bca7f9672f3ab4ea47611fd1fe1
Mayamii <51121925+Mayamii@users.noreply.github.com>
2020-06-21 17:48:00 +0200

    ò__o

--- src/Position.java
+++ src/Position.java
@@ -4,7 +4,10 @@ public final class Position
 {
     private final int _xpos;
     private final int _ypos;
-    static private Random _random = new Random();
+    private static Random _random = new Random();
+
+    private static final int FIELDWIDTH = 34;
+    private static final int FIELDHEIGHT = 23;
 
     public Position(int x, int y)
     {
@@ -25,7 +28,7 @@ public final class Position
     public Position moveRight()
     {
         int xpos = _xpos + 1;
-        if (xpos > 33)
+        if (xpos > FIELDWIDTH - 1)
         {
             xpos = 0;
         }
@@ -38,7 +41,7 @@ public final class Position
         int xpos = _xpos - 1;
         if (xpos < 0)
         {
-            xpos = 33;
+            xpos = FIELDWIDTH - 1;
         }
         return new Position(xpos, _ypos);
     }
@@ -48,7 +51,7 @@ public final class Position
         int ypos = _ypos - 1;
         if (ypos < 0)
         {
-            ypos = 22;
+            ypos = FIELDHEIGHT - 1;
         }
         return new Position(_xpos, ypos);
     }
@@ -56,7 +59,7 @@ public final class Position
     public Position moveDown()
     {
         int ypos = _ypos + 1;
-        if (ypos > 22)
+        if (ypos > FIELDHEIGHT - 1)
         {
             ypos = 0;
         }
@@ -65,6 +68,7 @@ public final class Position
 
     public static Position randomPos()
     {
-        return new Position(_random.nextInt(34), _random.nextInt(23));
+        return new Position(_random.nextInt(FIELDWIDTH),
+                _random.nextInt(FIELDHEIGHT));
     }
 }
