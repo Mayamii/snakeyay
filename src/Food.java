c8845b2b0125667ce9c989c0025d8e2b31748750
Mayamii <51121925+Mayamii@users.noreply.github.com>
2020-06-21 17:15:22 +0200

    Nullpointer Exception behoben Food Random

--- src/Food.java
+++ src/Food.java
@@ -12,6 +12,7 @@ public class Food
 
     public Food()
     {
+        _random = new Random();
         _xPos = _random.nextInt(34);
         _yPos = _random.nextInt(23);
         _pos = new Position(_xPos, _yPos);
