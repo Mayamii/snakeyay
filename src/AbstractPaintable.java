30b38359648c1e0ee83830f49c0c6315a647e0e2
Mayamii <51121925+Mayamii@users.noreply.github.com>
2020-06-21 19:10:15 +0200

    Abstrakte Klasse AbstractPaintable erstellt und Interface

--- src/AbstractPaintable.java
+++ src/AbstractPaintable.java
@@ -1,5 +1,17 @@
+import javax.swing.ImageIcon;
 
-public abstract class AbstractPaintable
+public abstract class AbstractPaintable implements Paintable
 {
+    private Position _position;
+    private ImageIcon _image;
 
+    public void paint()
+    {
+
+    }
+
+    public void update()
+    {
+
+    }
 }
